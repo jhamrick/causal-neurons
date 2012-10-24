f3a6d0c700e9a11878ef62a7e22467ad1d0f0a6c
Jessica B. Hamrick <jhamrick@mit.edu>
2012-10-24 00:57:30 -0700

    Clean up code, use more realistic parameters

--- two-neuron-network.py
+++ two-neuron-network.py
@@ -3,108 +3,172 @@
 
 # <codecell>
 
+#############
+## Imports ##
+#############
+
 import numpy as np
+import matplotlib.pyplot as plt
 
 # <codecell>
 
-nsteps = 100
-axx = 1.
-axy = 2.
-ayx = .2
-ayy = 1.
+###############
+## Constants ##
+###############
+
+TIMESTEP   =  1    # length of time for each step, ms
+REST_POT   = -70   # membrane resting potention, mV
+AP_THRESH  = -55   # action potential threshold, mV
+AP_POT     =  40   # action potential peak, mV
 
 # <codecell>
 
-zx = np.random.randn(nsteps) / 2.
-zy = np.random.randn(nsteps) / 2.
+################
+## Parameters ##
+################
 
-wxx = -20.
-wxy = 1.
-wyx = -0.5
-wyy = -10.
+nsteps = 100     # number of time steps to run simulation for
 
-xv = np.zeros(nsteps)
-xxd = np.zeros(nsteps)
-xyd = np.zeros(nsteps)
+rd     = 50.0    # refractory inhibition decay rate, mV / ms
+axy    =  2.0    # X->Y synaptic potential decay rate, mV / ms
+ayx    =  0.5    # Y->X synaptic potential decay rate, mV / ms
+
+wd     = -115.0  # refractory inhibition weight
+wxy    =  50.0   # X->Y synaptic weight
+wyx    = -25.0   # Y->X synaptic weight
+
+# <codecell>
+
+########################
+## Specify input data ##
+########################
+
+zx = np.random.randn(nsteps) * 10
+zy = np.random.randn(nsteps) * 10
+
+# <codecell>
+
+######################################
+## Allocate arrays for network data ##
+######################################
+
+# pre-threshold values for each neuron
+xv = np.zeros(nsteps) + REST_POT
+yv = np.zeros(nsteps) + REST_POT
+
+# post-threshold vlaues for each neuron
 x = np.zeros(nsteps)
-
-yv = np.zeros(nsteps)
-yxd = np.zeros(nsteps)
-yyd = np.zeros(nsteps)
 y = np.zeros(nsteps)
 
+# potential decay for each synapse
+xxd = np.zeros(nsteps)
+xyd = np.zeros(nsteps)
+yxd = np.zeros(nsteps)
+yyd = np.zeros(nsteps)
+
 # <codecell>
 
-plt.figure(1)
+#########################
+## Initialize plotting ##
+#########################
+
+# plotting parameters
+ylim = (-100, 100)
+xlim = (0, nsteps-1)
+nrow = 4
+ncol = 2
+
+def create_subplot(ix):
+    ax = plt.subplot(nrow, ncol, ix)
+    ax.set_xlim(*xlim)
+    ax.set_ylim(*ylim)
+    ax.grid(True)
+    if ((ix-1) % ncol) != 0:
+        ax.set_yticklabels([])
+    if (nrow*ncol - (ix-1)) > ncol:
+        ax.set_xticklabels([])
+    return ax
+
+# create figure
+plt.close('all')
+fig = plt.figure(1)
 plt.clf()
-plt.subplot(221)
-plt.xlim(0, nsteps-1)
-plt.ylim(-1, 1)
-plt.plot(np.arange(nsteps), zx, 'k')
-plt.subplot(222)
-plt.xlim(0, nsteps-1)
-plt.ylim(-1, 1)
-plt.plot(np.arange(nsteps), zy, 'k')
-plt.subplot(223)
-plt.xlim(0, nsteps-1)
-plt.ylim(-1, 1)
-plt.subplot(224)
-plt.xlim(0, nsteps-1)
-plt.ylim(-1, 1)
-plt.show()
-plt.draw()
-plt.draw()
+fig.set_figwidth(8)
+fig.set_figheight(10)
+
+# x input
+ax_zx = create_subplot(1)
+ax_zx.plot(np.arange(nsteps), zx, 'k')
+# y input
+ax_zy = create_subplot(2)
+ax_zy.plot(np.arange(nsteps), zy, 'k')
+# x value (pre-threshold)
+ax_xv = create_subplot(3)
+# y value (pre-threshold)
+ax_yv = create_subplot(4)
+# x value
+ax_x = create_subplot(5)
+ax_x.set_ylim(-0.05, 1.05)
+# y value
+ax_y = create_subplot(6)
+ax_y.set_ylim(-0.05, 1.05)
+# x potential decay
+ax_xd = create_subplot(7)
+# y potential decay
+ax_yd = create_subplot(8)
 
 # <codecell>
 
 for t in xrange(nsteps-1):
-    xv[t+1] = zx[t] + xxd[t] + yxd[t]
-    yv[t+1] = zy[t] + xyd[t] + yyd[t]
 
-    sl = slice(max(0, t-10), t+1)
-    nt = x[sl].size - 1
-    xf = np.nonzero(x[sl] == 1)[0]
-    yf = np.nonzero(y[sl] == 1)[0]
-    xxd[t+1] = wxx*np.sum(np.exp(-axx*(nt-np.nonzero(x[sl] == 1)[0])))
-    yxd[t+1] = wyx*np.sum(np.exp(-ayx*(nt-np.nonzero(y[sl] == 1)[0])))
-    xyd[t+1] = wxy*np.sum(np.exp(-axy*(nt-np.nonzero(x[sl] == 1)[0])))
-    yyd[t+1] = wyy*np.sum(np.exp(-ayy*(nt-np.nonzero(y[sl] == 1)[0])))
-    
-    if xv[t] > 0.5 and yv[t] > 0.5:
+    # update pre-threshold values
+    xv[t+1] = zx[t] + xxd[t] + yxd[t] + REST_POT
+    yv[t+1] = zy[t] + xyd[t] + yyd[t] + REST_POT
+
+    # generate an action potential, if necessary
+    if xv[t] > AP_THRESH and yv[t] > AP_THRESH:
         xfirst = np.random.rand() > 0.5
         print xfirst
     else:
         xfirst = True
     
-    if xv[t] > 0.5 and xfirst:
+    if xv[t] > AP_THRESH and xfirst:
         x[t+1] = 1
-        xv[t+1] = 0
+        xv[t] = AP_POT
+        xv[t+1] = REST_POT
 
-    elif yv[t] > 0.5:
+    elif yv[t] > AP_THRESH:
         y[t+1] = 1
-        yv[t+1] = 0
+        yv[t] = AP_POT
+        yv[t+1] = REST_POT
 
-    # print t
-    # print z[t]
-    # print xd[t+1], xv[t+1], x[t+1]
-    # print yd[t+1], yv[t+1], y[t+1]
-    # print
+    # update post-threshold membrane potential for each neuron 
+    sl = slice(max(0, t-10), t+1)
+    nt = x[sl].size - 1
+    xf = np.nonzero(x[sl] == 1)[0]
+    yf = np.nonzero(y[sl] == 1)[0]
 
+    xxd[t+1] = wd*np.sum(np.exp(-(rd*TIMESTEP)*(nt-np.nonzero(x[sl] == 1)[0]))) 
+    yyd[t+1] = wd*np.sum(np.exp(-(rd*TIMESTEP)*(nt-np.nonzero(y[sl] == 1)[0])))
+
+    yxd[t+1] = wyx*np.sum(np.exp(-(ayx*TIMESTEP)*(nt-np.nonzero(y[sl] == 1)[0])))
+    xyd[t+1] = wxy*np.sum(np.exp(-(axy*TIMESTEP)*(nt-np.nonzero(x[sl] == 1)[0])))
+
+    # plotting
     tv = [t, t+1]
-    plt.subplot(223)
-    plt.plot(tv, x[t:t+2], 'r-')
-    plt.plot(tv, xv[t:t+2], 'r--')
-    plt.subplot(224)
-    plt.plot(tv, y[t:t+2], 'b-')
-    plt.plot(tv, yv[t:t+2], 'b--')
-    plt.draw() 
+    ax_xv.plot(tv, xv[t:t+2], 'r-')
+    ax_x.plot(tv, x[t:t+2], 'r-')
+    ax_xd.plot(tv, xyd[t:t+2], 'r-')
+    ax_yv.plot(tv, yv[t:t+2], 'b-')
+    ax_y.plot(tv, y[t:t+2], 'b-')
+    ax_yd.plot(tv, yxd[t:t+2], 'b-')
 
-
-# <codecell>
-
-x
+plt.show()
 
 # <codecell>
 
  
+
+# <codecell>
+
 
